456ebbe152ad6ab471fcb659c8ce5edcc0e47ffc
helltime85 <waterland43@gmail.com>
2021-05-12 05:32:47 +0000

    mongoDB init

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+const mongoose = require('mongoose')
+
+const conUrl = "mongodb+srv://mongodb_user:nMhwHpCyKOsOgzEX@cluster0.bpwbn.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(url, {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+}).then(() => console.log("MongoDB Connect"))
+  .catch(err => console.log(err))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
